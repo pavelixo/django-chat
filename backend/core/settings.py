48a3a0f78f1a7479cec805066ed045dd9f29607e
pavelixo <trashxindoragon87@gmail.com>
2024-07-02 21:23:57 -0300

    vue

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -122,3 +122,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Channel
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
