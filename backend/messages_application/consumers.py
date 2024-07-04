a6d0a59fd58ff26c5f849dd7c2d16a1a7104c66b
pavelixo <trashxindoragon87@gmail.com>
2024-07-04 09:00:27 -0300

    ...

--- backend/messages_application/consumers.py
+++ backend/messages_application/consumers.py
@@ -1,5 +1,37 @@
-import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from random import choice
+from string import digits
+from channels.generic.websocket import AsyncWebsocketConsumer, AsyncJsonWebsocketConsumer
+
+
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+  async def connect(self):
+    await self.accept()
+    request_user = self.scope['user']
+
+    if request_user.is_anonymous:
+      identifier = "".join(choice(digits) for _ in range(4))
+      self.partial_user = { 
+        'id': None, 
+        'username': f'anonymous#{identifier}' 
+      }
+    
+    else:
+      self.partial_user = { 
+        'id': request_user.id, 
+        'username': request_user.username 
+      }
+  
+  async def disconnect(self, code):
+    pass
+
+  async def receive_json(self, content, **kwargs):
+    message = content['message']
+    
+    if not len(message) > 1000:
+      await self.send_json({ 
+        'user': self.partial_user, 
+        'message': message 
+      })
 
 
 class HealthCheckConsumer(AsyncWebsocketConsumer):
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,9 @@
-<template>
+<script setup>
+  import { RouterView } from 'vue-router'
+</script>
 
+<template>
+  <main>
+    <RouterView />
+  </main>
 </template>
